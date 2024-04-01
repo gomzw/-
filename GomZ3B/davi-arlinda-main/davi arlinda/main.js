5497b03f00d6c3b79b91ed67400acbb938ebb32b
gomzw <111518206+gomzw@users.noreply.github.com>
2024-04-01 11:04:10 -0300

    Add files via upload

---
+++ GomZ3B/davi-arlinda-main/davi arlinda/main.js
@@ -0,0 +1,36 @@
+const botoes = document.querySelectorAll(".botao");
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
+}
+
+
+
+
+const contadores = document.querySelectorAll(".contador");
+const tempoOobjetivo1 = new Date (" 25-10-05t00:00:00");
+const tempoObjetivo2 = new Date  ("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date ("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date ("2024-02-01T00:00:00");
+
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+
+function calculaTempo(tempoObjetivo){
+  let tempoAtual = newDate ();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let
+
+
+
+
+
+}
